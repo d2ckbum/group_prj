a7781b6d4366789ce72e06f5d2ce2947eb272dca
JHyun12864 <hyeon12864@naver.com>
2025-03-31 12:38:58 +0900

    hjy

---
+++ group_prj/src/kr/co/sist/hjy/Hjy.java
@@ -0,0 +1,11 @@
+package kr.co.sist.hjy;
+
+public class Hjy {
+
+	public static void main(String[] args) {
+		System.out.println("자신이 구현해야할 코드를 작성........");
+		System.out.println("나는 HJY야~~!!");
+
+	}//main
+
+}//class
