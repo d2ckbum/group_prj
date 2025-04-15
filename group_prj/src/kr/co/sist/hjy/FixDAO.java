cbdb0546985b75d102e12a31a80167a9e3a19026
JHyun12864 <hyeon12864@naver.com>
2025-04-15 13:06:37 +0900

    2025 04 15 오후 1:6 현지영

--- group_prj/src/kr/co/sist/hjy/FixDAO.java
+++ group_prj/src/kr/co/sist/hjy/FixDAO.java
@@ -109,10 +109,10 @@ public class FixDAO {
 		StringBuilder selectAllFix=new StringBuilder();
 		selectAllFix
 		.append("	select	f.FIX_NUM, f.FIX_REG_DATE,  f.ITEM_NAME,   			")
-		.append("	mf.mfg_name,   c.CAR_TYPE,   m.mem_num,   m.MEM_NAME,		")
+		.append("	mf.mfg_name,   c.CAR_TYPE,   f.mem_num,   m.MEM_NAME,		")
 		.append(" f.total_price,   f.fix_status, f.fix_memo, m.mem_tell 		")
-		.append("	from	fix f, item i, car c, member m,manufacturer mf		")
-		.append("	where (f.mem_num=m.mem_num and m.car_num=c.car_num and m.mfg_num=mf.mfg_num)		")
+		.append("	from	fix f, car c, member m, manufacturer mf		")
+		.append("	where f.mem_num=m.mem_num and m.car_num=c.car_num  and m.mfg_num=mf.mfg_num		")
 		.append("   order by f.fix_num desc")
 		;
 		
